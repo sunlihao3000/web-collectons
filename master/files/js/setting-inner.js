ecc917be44c3bf7ab0d017fb3229e110522fa695
bobby sun <sunlihao3000@hotmail.com>
2015-10-14 16:33:34 +0100

    chanegs

---
+++ master/files/js/setting-inner.js
@@ -0,0 +1,18 @@
+
+
+(function(){
+  "use strict";
+
+  	$(document).ready(function($){
+      
+
+	}); // end document load 
+
+}()); // end function strict
+
+
+$(window).load(function() {
+
+});
+
+//
