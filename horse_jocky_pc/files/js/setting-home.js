b92ca86404a26545b1909012465c8bc995ab1a04
bobby sun <sunlihao3000@hotmail.com>
2015-10-20 10:43:21 +0100

    del and change 20.10.2015

---
+++ horse_jocky_pc/files/js/setting-home.js
@@ -0,0 +1,28 @@
+
+(function(){
+
+  "use strict";
+  
+  	$(document).ready(function($){
+
+
+
+
+
+
+
+    });// 
+
+}());  // end function strict
+
+
+
+
+
+// makes sure the whole site is loaded
+$(window).load(function () {
+
+
+
+});
+
